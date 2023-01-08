cbaeb9114ab0cbe4ce6bd8896d6e727c57981aef
mahadidev7 <mahadidev7@gmail.com>
2023-01-09 00:55:33 +0600

    mango mango

--- app/components/home/Service.js
+++ app/components/home/Service.js
@@ -3,7 +3,7 @@ import React from "react";
 import tw from "twrnc";
 import ServiceItem from "./ServiceItem";
 
-const Service = () => {
+const Service = ({navigation}) => {
   return (
     <View style={tw`bg-gray-100 flex-row flex-wrap justify-between items-center py-5 px-4 `}>
       <ServiceItem
@@ -11,12 +11,14 @@ const Service = () => {
         color="orange"
         name="Send Money"
         description="To acc to acc"
+        navigator={() => navigation.navigate("SendMoneyScreen")}
       />
       <ServiceItem
         icon="money"
         color="green"
         name="Recive Money"
         description="Manage Account"
+        navigator={() => navigation.navigate("SurahListScreen")}
       />
     </View>
   );
--- app/components/home/ServiceItem.js
+++ app/components/home/ServiceItem.js
@@ -3,10 +3,11 @@ import React from "react";
 import tw from "twrnc";
 import FontAwesome from "react-native-vector-icons/FontAwesome";
 
-const ServiceItem = ({ icon, color, name, description }) => {
+const ServiceItem = ({ icon, color, name, description, navigator }) => {
   return (
     <TouchableOpacity
       style={tw`w-[48%] border border-gray-100 rounded-5 p-4 py-6 bg-white`}
+      onPress={navigator}
     >
       <View style={tw`bg-gray-100 w-15 h-15 rounded-full justify-center items-center`}>
         <Text>
