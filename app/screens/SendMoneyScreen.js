eb51a0a0eec6c9d462218f6dd7a96b7df4b3d50d
morshidulrahman <boydanger416@gmail.com>
2023-01-09 00:53:28 +0600

    send-button

--- app/screens/SendMoneyScreen.js
+++ app/screens/SendMoneyScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, TextInput } from 'react-native'
+import { View, Text, TextInput, Button } from 'react-native'
 import React from 'react'
 import SafeAreaWrapper from '../configs/SafeAreaWrapper'
 
@@ -7,27 +7,34 @@ const SendMoneyScreen = () => {
         <SafeAreaWrapper >
             <View className="items-center justify-center">
                 <Text className="text-2xl capitalize font-semibold py-1">
-                    sending
+                    send money
                 </Text>
             </View>
-            <View className="p-4 w-[70%] bg-white ">
+            <View className="p-4 w-full bg-white ">
                 <View className="">
                     <Text className="font-semibold pb-2 text-lg">Enter email</Text>
                     <TextInput
-                        className="rounded-sm border px-2 py-1"
+                        className="rounded-md border px-2 py-1"
                         placeholder='enter your email'
                         placeholderTextColor="gray"
                         autoFocus={true}
                     />
                 </View>
-                <View className="py-2">
+                <View className="py-2 mb-4">
                     <Text className="font-semibold pb-2 text-lg">Enter Amount</Text>
                     <TextInput
-                        className="rounded-sm border px-2 py-1"
+                        className="rounded-md border px-2 py-1"
                         placeholder='enter your email'
                         placeholderTextColor="gray"
                         autoFocus={true}
                         keyboardType='numeric'
+                    />
+                </View>
+                <View >
+                    <Button
+                        className="rounded-md py-2"
+                        title="send"
+                        color="#0081C9"
                     />
                 </View>
             </View>
